638223cebff74002799191c2fa9f3aa312c2cef1
fritz-max <fritz.doermann@gmail.com>
2020-10-05 23:09:00 +0200

    added testloop for 3 different noise levels

--- src/simpleDPClassifier.py
+++ src/simpleDPClassifier.py
@@ -25,22 +25,23 @@ class Model(nn.Module):
             weight_decay=params['l2_penalty'],
         )
         self.criterion = nn.NLLLoss()
+        self.test_results = []
 
-        print("Specified parameters achieve ({}, {})-DP".format(
-            analysis.epsilon(
-                len(X_train),
-                params['minibatch_size'],
-                params['noise_multiplier'],
-                params['iterations'],
-                params['delta']
-            ),
-            params['delta'],
-        ))
+        self.dp = (analysis.epsilon(
+                        len(X_train),
+                        params['minibatch_size'],
+                        params['noise_multiplier'],
+                        params['iterations'],
+                        params['delta']
+                    ),
+                    params['delta'])
+
+        print(f"Specified parameters achieve ({self.dp[0]:.2f}, {self.dp[1]})-DP")
 
     def forward(self, x):
         return self.model(x)
 
-    def train(self, dataset):
+    def train(self, dataset, test_loader):
         minibatch_loader, microbatch_loader = sampling.get_data_loaders(
             self.params['minibatch_size'],
             self.params['microbatch_size'],
@@ -52,6 +53,7 @@ class Model(nn.Module):
         for X_minibatch, y_minibatch in minibatch_loader(dataset):
             self.optimizer.zero_grad()
             running_loss = 0
+            iteration += 1
             for X_microbatch, y_microbatch in microbatch_loader(TensorDataset(X_minibatch, y_minibatch)):
                 X_microbatch = X_microbatch.to(self.params['device'])
                 y_microbatch = y_microbatch.to(self.params['device'])
@@ -69,23 +71,22 @@ class Model(nn.Module):
             if iteration % 10 == 0:
                 print('[Iteration %d/%d] [Avg Loss (last 10 It.): %f] [This It. Loss: %f]' % 
                             (iteration, self.params['iterations'],
-                            accum_loss/(self.params['minibatch_size']/self.params['microbatch_size']/10*self.params['minibatch_size']),
+                            accum_loss/(10*(self.params['minibatch_size']/self.params['microbatch_size'])),
                             running_loss/(self.params['minibatch_size']/self.params['microbatch_size'])))
                 accum_loss = 0
 
-            iteration += 1
-        
-        # for i in range(epochs):
-        #     running_loss = 0
-        #     for X, y in dataloader:
-        #         self.optimizer.zero_grad()
-        #         prediction = self.model(X)
-        #         loss = self.criterion(prediction, y)
-        #         loss.backward()
-        #         self.optimizer.step()
-        #         running_loss += loss.item()
-        #     else:
-        #         print("Training loss: ", (running_loss/len(dataloader)))
+            if iteration % 100 == 0:
+                print("Finished an Epoch (100 Minibatches of size L)")
+                test_acc = self.eval_dataloader(dataloader=test_loader)
+                self.test_results.append(test_acc)
+
+    
+    def eval_dataset(self, X_test, y_test):
+        with torch.no_grad():
+            logps = self.model(X_test.view(-1, params['pca_components']))
+        print(f"Accuracy: {round(((torch.exp(logps).argmax(dim=1) == y_test).sum().float()/len(X_test)*100).item(), 2)}%")
+        print(f"With ({self.dp[0]:.2f}, {self.dp[1]})-DP")
+
 
     def eval_dataloader(self, dataloader):
         accuracies = []
@@ -95,35 +96,44 @@ class Model(nn.Module):
             accuracy = ((torch.exp(logps).argmax(dim=1) ==
                          y_test).sum().float()/len(X_test)*100).item()
             accuracies.append(accuracy)
+        
+        test_acc = round(sum(accuracies)/len(accuracies), 2)
         print(
-            f"Accuracy: {round(sum(accuracies)/len(accuracies), 2)}%")
-    
-    def eval_dataset(self, X_test, y_test):
-        with torch.no_grad():
-            logps = self.model(X_test.view(-1, params['pca_components']))
-        print(f"Accuracy: {round(((torch.exp(logps).argmax(dim=1) == y_test).sum().float()/len(X_test)*100).item(), 2)}%")
+            f"Test Accuracy: {test_acc}%")
+
+        return test_acc
+
+
 
 if __name__ == '__main__':
-    # mnist_train_loader, mnist_test_loader = mnist_private_pca_dataloaders(
-    #     epsilon=1)
-    params = {
-        'delta': 1e-5,
-        'device': 'cpu',
-        'iterations': 100,
-        'l2_norm_clip': 4.,
-        'l2_penalty': 0.001,
-        'lr': 0.075,
-        'microbatch_size': 1,
-        'minibatch_size': 200,
-        'noise_multiplier': 2,
-        'pca_components': 30
-    }
+    noise_levels = [2, 4, 8]
 
-    X_train, y_train, X_test, y_test = mnist_private_pca(epsilon=10, components=params['pca_components'])
-    
-    trainset = TensorDataset(X_train, y_train)
-    # testset = TensorDataset(X_test, y_test)
+    for noise in noise_levels:
+        params = {
+            'delta': 1e-5,
+            'device': 'cpu',
+            'iterations': 10000,
+            'l2_norm_clip': 4.,
+            'l2_penalty': 0.001,
+            'lr': 0.075,
+            'microbatch_size': 1,
+            'minibatch_size': 600,
+            'noise_multiplier': noise,
+            'pca_components': 60
+        }
 
-    model = Model(params=params)
-    model.train(dataset=trainset)
-    model.eval_dataset(X_test, y_test)
+        X_train, y_train, X_test, y_test = mnist_private_pca(epsilon=1, components=params['pca_components'])
+        
+        test_loader=torch.utils.data.DataLoader(TensorDataset(X_test, y_test), batch_size=64, shuffle=True)
+
+        model = Model(params=params)
+        model.train(dataset=TensorDataset(X_train, y_train), test_loader=test_loader)
+        model.eval_dataset(X_test, y_test)
+
+        with open(f"data_noise{noise}.txt", 'w') as filehandle:
+            filehandle.write(f"Noise Level: {noise}\nDP: ({model.dp[0]:.2f}, {model.dp[1]})\n")
+            filehandle.write("\n")
+            filehandle.write("Test Accuracies (every 100 Iterations, i.e. 1 Epoch):\n")
+
+            for i, elem in enumerate(model.test_results):
+                filehandle.write(f"{i}: {elem}\n")
