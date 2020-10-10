af42ba46175bcde0659b1df35b32fde4293c7873
Osvald Frisk <osvald.frisk@gmail.com>
2020-10-10 20:57:44 +0200

    added comments to DPClassifier class

--- src/DPClassifier.py
+++ src/DPClassifier.py
@@ -1,6 +1,8 @@
 import torch
 
 class Model(torch.nn.Module):
+    '''Simple dense model, with one hidden layer
+    '''
     def __init__(self, input_dim):
         super(Model, self).__init__()
         self.model = torch.nn.Sequential(
@@ -11,12 +13,21 @@ class Model(torch.nn.Module):
         ).to('cpu')
 
     def forward(self, X):
+        '''Used to propagate the features through the model to later update
+        model by backpropagation
+        '''
         return self.model(X)
 
     def predict(self, X):
+        '''Use model to predict labels given that input features X. Returns
+        the probabilities of each sample being each class.
+        '''
         with torch.no_grad():
             logps = self.model(X)
         return torch.exp(logps)
 
     def predict_class(self, X):
+        '''Use model to predict labels given that input features X. Returns
+        predicted class.
+        '''
         return self.predict(X).argmax(dim=1)
